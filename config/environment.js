56b94f9d99184f539d3367a2293cd3ad8b422057
Lili Boxer <ninaliboxer@gmail.com>
2019-12-03 15:39:53 -0800

    mapbox access token

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,7 @@ module.exports = function(environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibGlsaWJveGVyIiwiYSI6ImNrM3FpNTJxbDAxeGozanF1YWtoczFnMngifQ.6XOVsa5bmd3vE62hRO7iow';
+
   return ENV;
 };
